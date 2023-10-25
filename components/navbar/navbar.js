104149c4de21365fb34aae18f83204ad7d45e9f4
Alex <alex.j.harker@gmail.com>
2023-10-25 12:53:41 -0500

    Update navbar color

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -28,14 +28,14 @@ export default function Navbar() {
   }, [router.pathname]);
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-black">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -53,7 +53,7 @@ export default function Navbar() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-800 text-white' : 'text-gray-300 hover:bg-gray-800 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -66,7 +66,7 @@ export default function Navbar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <a href="https://github.com/GumboLimbo" target='_blank'
-                    className="relative rounded-full bg-gray-800 p-2 text-gray-400 hover:text-white"
+                    className="relative rounded-full bg-inherit p-2 text-gray-400 hover:text-white"
                 >
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">View notifications</span>
@@ -74,7 +74,7 @@ export default function Navbar() {
                 </a>
 
                 <a href="https://www.linkedin.com/in/alexander-harker/" target='_blank'
-                    className="relative rounded-full bg-gray-800 p-2 text-gray-400 hover:text-white"
+                    className="relative rounded-full bg-inherit p-2 text-gray-400 hover:text-white"
                 >
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">View notifications</span>
