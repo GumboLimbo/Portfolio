0895dadca36692ed4e633bfcdc1cb6d043f710f4
Alex <alex.j.harker@gmail.com>
2023-11-03 17:19:46 -0500

    Attempt to make hover text mobile compatible - untested

--- components/HoverText/HoverText.js
+++ components/HoverText/HoverText.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config.js'
+
 export default function HoverText({ title, innerText }) {
+    const fullConfig = resolveConfig(tailwindConfig);
+    const isMobile = useMediaQuery({ query: `(max-width: ${fullConfig.theme.screens.sm})` });
+
     let duration = "duration-[1000ms]";
+    let [open, setOpen] = useState(false);
+
+    function handleClick() {
+        setOpen(!open);
+    }
+
+    if (isMobile) {
+
+        return (
+            <div className="grid grid-cols-1 grid-rows-1 group overflow-hidden">
+                <div onClick={handleClick} className={`animate-pulse col-start-1 row-start-1 transition-all ${duration} ${open ? 'translate-x-[-100%]' : ''} md:text-4xl`}>{title}</div>
+                <div onClick={handleClick} className={`mx-[5%] col-start-1 row-start-1 transition-all ${duration} ${open ? 'translate-x-0' : 'translate-x-[200%]'}`}>
+                    {innerText}
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="grid grid-cols-1 grid-rows-1 group overflow-hidden">
-            <div className={`animate-pulse col-start-1 row-start-1 transition-all ${duration} group-hover:translate-x-[-100%] md:text-4xl`}>{title}</div>
-            <div className={`col-start-1 row-start-1 transition-all ${duration} group-hover:translate-x-0 translate-x-[100%]`}>
+            <div onClick={handleClick} className={`animate-pulse col-start-1 row-start-1 transition-all ${duration} md:group-hover:translate-x-[-100%] md:text-4xl`}>{title}</div>
+            <div onClick={handleClick} className={`mx-[5%] col-start-1 row-start-1 transition-all ${duration} md:group-hover:translate-x-0 translate-x-[200%]`}>
                 {innerText}
             </div>
         </div>
