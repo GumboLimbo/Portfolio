7e29e1359595522f57aa46ba4ba109fe78c1d17d
Alex <alex.j.harker@gmail.com>
2023-11-02 19:59:15 -0500

    Fill in shade exp

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import Head from 'next/head';
 
 export default function Home() {
   const experience = [
-    { name: 'Shade Protocol', fromColor: "from-purple-300", toColor: "to-purple-500", description: "", techStack: "", github: "" },
-    { name: 'Infinite Campus', fromColor: "from-green-300", toColor: "to-green-500", description: "", techStack: "", github: "" },
-    { name: 'This Website', fromColor: "from-gray-300", toColor: "to-gray-500", description: "", techStack: "", github: "" },
-    { name: 'Arbitrage Bot', fromColor: "from-yellow-300", toColor: "to-yellow-500", description: "", techStack: "", github: "" },
+    { name: 'Shade Protocol\'s ShadeSwap', fromColor: "from-purple-300", toColor: "to-purple-500", description: "Researched, built, and managed the audit of a crypto exchange. Successfully managed > $10M. Also built supporting cloud infrastructure and db.", techStack: "Rust, Smart Contracts, AWS, PostgresDB, Terraform, Vue.js, Python", github: "https://github.com/securesecrets/shadeswap", inAction: "https://app.shadeprotocol.io/swap/", paper: "https://arxiv.org/abs/2302.07822" },
+    { name: 'Infinite Campus', fromColor: "from-green-300", toColor: "to-green-500", description: "", techStack: "", github: "", inAction: "", paper: "" },
+    { name: 'This Website', fromColor: "from-gray-300", toColor: "to-gray-500", description: "", techStack: "", github: "", inAction: "", paper: "" },
+    { name: 'Arbitrage Bot', fromColor: "from-yellow-300", toColor: "to-yellow-500", description: "", techStack: "", github: "", inAction: "", paper: "" },
   ];
 
   return (
@@ -42,7 +42,7 @@ export default function Home() {
               </div>
               <div className='grid grid-cols-1 md:grid-cols-2 justify-center pt-10 pb-20 md:pb-0'>
                 {experience.map((exp) => (
-                  <div className={`transition-all duration-[200ms] ${exp.fromColor} ${exp.toColor} justify-self-center hover:shadow-xl w-11/12 h-5/6 hover:scale-105 hover:scale-105 mx-2 my-4 bg-gradient-to-br rounded-md`}>
+                  <div className={`transition-all duration-[200ms] ${exp.fromColor} ${exp.toColor} justify-self-center hover:shadow-xl w-11/12 max-h-fit hover:scale-105 hover:scale-105 mx-2 my-4 bg-gradient-to-br rounded-md`}>
                     <div className='grid grid-cols-3 min-h-[48px]'>
                       <a href={exp.github} target='_blank'
                         className="relative rounded-full py-1 px-2 text-gray-400 hover:text-white"
@@ -52,12 +52,24 @@ export default function Home() {
                       <div className='text-2xl'>
                         {exp.name}
                       </div>
+                      <div className='justify-self-end text-sm'>
+                        {exp.inAction &&
+                          <a href={exp.inAction} target='_blank'
+                            className="flex transition-all duration-[200ms] max-h-min max-w-fit text-center align-items-center rounded-md my-1 mx-2 text-gray-800 hover:text-white hover:bg-purple-800">
+                            See it in action!</a>
+                        }
+                        {exp.paper &&
+                          <a href={exp.paper} target='_blank'
+                            className="flex transition-all duration-[200ms] max-h-min max-w-fit text-center align-items-center rounded-md my-1 mx-2 text-gray-800 hover:text-white hover:bg-purple-800">
+                            Research Paper</a>
+                        }
+                      </div>
                     </div>
                     <div className='text-xl'>
-                      Description: {exp.description}
+                      {exp.description}
                     </div>
-                    <div className='text-xl'>
-                      Tech Stack: {exp.techStack}
+                    <div className='text-md border-t border-black mx-5'>
+                      {exp.techStack}
                     </div>
                   </div>
                 ))}
